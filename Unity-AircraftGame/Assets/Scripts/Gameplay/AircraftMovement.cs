1c33cd09bb0330eee21fd7f97d0a7b6199bb6870
frankvega2010 <franco_vega@msn.com>
2019-04-29 09:49:17 -0300

    Cleaned unused code

--- Unity-AircraftGame/Assets/Scripts/Gameplay/AircraftMovement.cs
+++ Unity-AircraftGame/Assets/Scripts/Gameplay/AircraftMovement.cs
@@ -8,20 +8,10 @@ public class AircraftMovement : MonoBehaviour
     public RawImage floorLocation;
     public GameObject aimedObject;
     public GameObject aircraftModel;
-    public int value;
-    public float upLimit1; //300
-                           // public float upLimit2 = 200; //220
-    public float downLimit1; //70
-                             // public float downLimit2 = 180; //200
     public float giro;
     public float inclination;
-    public bool switchOnce1 = false;
-    public bool switchOnce2 = false;
-    public bool isSwitching = false;
-    public Vector3 jetUp;
 
     private float tiltAroundZ;
-    //private float tiltAroundX;
     private float smooth;
     private Vector3 mousePos;
     private Vector3 v3;
@@ -33,7 +23,6 @@ public class AircraftMovement : MonoBehaviour
 
     private void Start()
     {
-        jetUp = new Vector3(0, 1, 0);
         smooth = 3.0f;
         inclination = 0;
         jet = JetStatus.Get();
@@ -45,7 +34,6 @@ public class AircraftMovement : MonoBehaviour
     {
         tiltAroundZ = Input.GetAxisRaw("Horizontal");
         GetAxisForward = Input.GetAxis("Vertical");
-        // tiltAroundX = Input.GetAxis("Mouse Y");
         mousePos = Input.mousePosition;
         v3 = Input.mousePosition;
         dir = transform.position - aimedObject.transform.position;
@@ -53,70 +41,6 @@ public class AircraftMovement : MonoBehaviour
         transform.position = transform.position - dir * 0.5f * (jet.speed * 0.1f) * Time.deltaTime;
         v3.z = 30.0f;
         aimedObject.transform.position = Camera.main.ScreenToWorldPoint(v3);
-
-        //Angle Check --START--
-        //isSwitching = false;
-        //if (transform.rotation.eulerAngles.x > upLimit1)
-        //{
-        //    isSwitching = true;
-        //    if (!switchOnce1)
-        //    {
-        //        value = value * (-1);
-        //        //giro = 1;
-        //        Debug.Log(transform.rotation.eulerAngles.x);
-        //        Debug.Log("z1 " + transform.rotation.eulerAngles.z);
-        //        switchOnce1 = true;
-        //        switchOnce2 = false;
-        //    }
-        //    //SetLookRotation.transform.eulerAngles = new Vector3(SetLookRotation.transform.rotation.eulerAngles.x, upLimit1, SetLookRotation.transform.rotation.eulerAngles.z);
-        //    jetUp = new Vector3(0, value, 0);
-        //}
-        //else if (transform.rotation.eulerAngles.x > downLimit1)
-        //{
-        //    isSwitching = true;
-        //    if (!switchOnce2)
-        //    {
-        //        //giro = 1;
-        //        value = value * (-1);
-        //        switchOnce1 = false;
-        //        switchOnce2 = true;
-        //        Debug.Log("2: " + transform.rotation.eulerAngles.x);
-        //        Debug.Log("2z " + transform.rotation.eulerAngles.z);
-        //    }
-        //    // SetLookRotation.transform.eulerAngles = new Vector3(SetLookRotation.transform.rotation.eulerAngles.x, downLimit1, SetLookRotation.transform.rotation.eulerAngles.z);
-        //    jetUp = new Vector3(0, value, 0);
-        //}
-
-        //if (!isSwitching)
-        //{
-        //    if (switchOnce2)
-        //    {
-        //        switchOnce2 = false;
-        //    }
-
-        //    if (switchOnce1)
-        //    {
-        //        switchOnce1 = false;
-        //    }
-        //}
-        //Angle Check --END--
-        //Debug.Log("something: " + transform.rotation.eulerAngles.x);
-
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            value = value * (-1);
-            jetUp = new Vector3(0, value, 0);
-            Debug.Log(value);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            value = value * (-1);
-            jetUp = new Vector3(0, value, 0);
-            Debug.Log(value);
-            //Debug.Log("inclinado: " + SetLookRotation.transform.rotation.eulerAngles.y);
-            //jet.speed
-        }
 
         if(GetAxisForward > 0)
         {
@@ -141,7 +65,6 @@ public class AircraftMovement : MonoBehaviour
             }
         }
 
-
         Quaternion q01 = Quaternion.identity;
         q01.SetLookRotation(aimedObject.transform.position - transform.position, transform.up); // similar to LookRotation
         transform.rotation = q01;
@@ -153,7 +76,7 @@ public class AircraftMovement : MonoBehaviour
         }
         else if (tiltAroundZ < 0)
         {
-            giro += 10 * Time.deltaTime;
+            giro += 10 * Time.deltaTime; // Usar rotate?
             //Debug.Log("inclinado: " + SetLookRotation.transform.rotation.eulerAngles.y);
         }
         else if(tiltAroundZ == 0)
@@ -175,37 +98,13 @@ public class AircraftMovement : MonoBehaviour
             //Debug.Log("tabla sube: " + inclination);
         }
 
-        //if (mousePos.y > Screen.height / 2) // tabla sube
-        //{
-        //    //Debug.Log("tabla sube: " + inclination);
-        //    inclination = (((transform.localEulerAngles.x + 360) % 360));
-        //}
-        //else if (mousePos.y < Screen.height / 2) // tabla baja
-        //{
-            
-        //    //Debug.Log("tabla baja: " + inclination);
-        //    inclination = (((transform.localEulerAngles.x + 360) % 360) - 360);
-        //}
-
-
-        //Debug.Log("rotacion x: " + transform.localEulerAngles.x);
-        //if(inclination < 0)
-        //{
-        //    //localRotX = (transform.localEulerAngles.x + 360) % 360;
-        //    inclination = (((transform.rotation.eulerAngles.x + 360) ) * 0.1f) * (-1);
-        //}
-        //else
-        //{
-        //    inclination = (transform.rotation.eulerAngles.x * 0.1f) * (-1);
-        //}
-
         jet.altitude = transform.position.y;
 
         
         floorLocation.rectTransform.rotation = Quaternion.Euler(new Vector3(0, 0, transform.rotation.eulerAngles.z * (-1)));
         floorLocation.rectTransform.localPosition = new Vector3(0, inclination, 0);
         transform.rotation = Quaternion.Slerp(aimedObject.transform.rotation, transform.rotation, Time.deltaTime * smooth);
-        transform.rotation = transform.rotation * Quaternion.Euler(new Vector3(0, 0, giro));
+        transform.rotation = transform.rotation * Quaternion.Euler(new Vector3(0, 0, giro)); // Usar rotate?
         aimedObject.transform.rotation = transform.rotation;
         
 
